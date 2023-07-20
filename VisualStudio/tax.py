dadf0aa01e0f9a13df90490f1c5ca2190f484206
Michał Woi <hersztgamer@gmail.com>
2023-07-20 14:29:48 +0200

    14

---
+++ VisualStudio/tax.py
@@ -0,0 +1,12 @@
+tax=0.07
+amount = float(10)
+total=amount+amount*tax
+print(total)
+
+name = 'Sarah'
+print(name)
+store_name = "Sarah's Store"
+print(store_name)
+hello = "Hello"
+greeting = hello + " "+ name
+print(greeting)
